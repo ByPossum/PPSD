3378c09274397afa7d37eedd509a61e3e92302be
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-11-30 15:40:14 +0000

    Basic implementation of vert reading.
    TODO:
    Add the rest pose comparitor.
    Implement 2D Colour array for deformations

---
+++ PPSD/Assets/Scripts/VertexOutput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexOutput : MonoBehaviour
+{
+
+    [SerializeField] private SkinnedMeshRenderer smr;
+    public void GenerateColourArray()
+    {
+        Vector3[] verts = smr.sharedMesh.vertices;
+        smr.BakeMesh(smr.sharedMesh);
+        Vector3[] currentPose = smr.sharedMesh.vertices;
+        Color[] map = new Color[verts.Length];
+        for(int i = 0; i < verts.Length; i++)
+        {
+            Debug.Log(string.Format("Rest Pose {0} Animated Pose {1}", verts[i], currentPose[i]));
+        }
+    }
+}
