bd8d7dbca63be7facc24c44f8ace7ceb1fd607ad
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-12-02 14:33:30 +0000

    Displacement Material Added

--- PPSD/Assets/Scripts/VertexOutput.cs
+++ PPSD/Assets/Scripts/VertexOutput.cs
@@ -8,64 +8,52 @@ public class VertexOutput : MonoBehaviour
 {
     [SerializeField] private bool vertTest;
     [SerializeField] private SkinnedMeshRenderer smr;
-    [SerializeField] GameObject meshPoints;
     [SerializeField] Material restMat;
     [SerializeField] Material animat;
     private int disRow;
     Vector3[] restPose;
-    GameObject[] restPosePoints;
-    GameObject[] animPosePoints;
     private void Start()
     {
         restPose = smr.sharedMesh.vertices;
-        restPosePoints = new GameObject[restPose.Length];
     }
     public void GenerateColourArray()
     {
+        // For instance the current pose
         Mesh newMesh = new Mesh();
         smr.BakeMesh(newMesh);
         Vector3[] currentPose = newMesh.vertices;
-        if(animPosePoints != null)
-            DeleteArrays();
-        restPosePoints = new GameObject[restPose.Length];
-        animPosePoints = new GameObject[currentPose.Length];
-        Debug.Log(restPose.Length);
         int poseWidth = Mathf.RoundToInt(Mathf.Sqrt(restPose.Length));
         int poseHeight = Mathf.Sqrt(restPose.Length) % 2 == 1 ? Mathf.RoundToInt(Mathf.Sqrt(restPose.Length)) : Mathf.RoundToInt(Mathf.Sqrt(restPose.Length)) + 1;
         Texture2D defColours = new Texture2D(poseWidth, poseWidth, TextureFormat.RGBA32, false);
-        Debug.Log(poseWidth);
         Color[,] map = new Color[poseWidth, poseWidth];
-        for(int i = 0; i < poseWidth; i++)
-        {
-            for (int j = 0; j < poseWidth; j++)
-            {
-                Color deformation = new Color(restPose[i].x - currentPose[i].x, restPose[i].y - currentPose[i].y, restPose[i].z - currentPose[i].z);
-                map[i, j] = deformation;
-                defColours.SetPixel(i, j, map[i, j]);
-            }
-        }
+        map = PaintDeformations(poseWidth, poseHeight, currentPose, defColours);
+        defColours.Apply();
+        Material colourMap = new Material(Shader.Find("Standard"));
+        colourMap.SetTexture("_MainTex", defColours);
+        smr.sharedMaterial.EnableKeyword("_MAINTEX");
+        smr.sharedMaterial.SetTexture("_MainTex", defColours);
         File.WriteAllBytes(Application.dataPath + "/Poses2D/displacements.png", defColours.EncodeToPNG());
         Debug.Log("Displacements Mapped");
     }
 
-    private void DeleteArrays()
+    private Color[,] PaintDeformations(int _i_poseWidth, int _i_poseHeight, Vector3[] _A_currentPose, Texture2D _tex_outputTexture)
     {
-        for (int i = 0; i < animPosePoints.Length; i++)
-            Destroy(animPosePoints[i]);
-        for (int j = 0; j < restPosePoints.Length; j++)
-            Destroy(restPosePoints[j]);
-    }
-}
-/*
- *             if (vertTest)
+        Color[,] newMap = new Color[_i_poseWidth, _i_poseHeight];
+        for (int i = 0; i < _i_poseWidth; i++)
+        {
+            for (int j = 0; j < _i_poseWidth; j++)
             {
-                GameObject ap = Instantiate(meshPoints);
-                ap.GetComponentInChildren<MeshRenderer>().sharedMaterial = restMat;
-                ap.transform.position = currentPose[i];
-                animPosePoints[i] = ap;
-                GameObject rp = Instantiate(meshPoints);
-                rp.GetComponentInChildren<MeshRenderer>().sharedMaterial = animat;
-                rp.transform.position = restPose[i];
-                restPosePoints[i] = rp;
+                Color deformation = new Color(_A_currentPose[i].x - restPose[i].x, _A_currentPose[i].y - restPose[i].y, _A_currentPose[i].z - restPose[i].z);
+                newMap[i, j] = deformation;
+                _tex_outputTexture.SetPixel(i, j, newMap[i, j]);
             }
- */
+        }
+        return newMap;
+    }
+
+    private void PaintUVs()
+    {
+
+    }
+
+}
