4aa9d4420335680dceb17e560a67c02f8f679fec
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2021-02-16 17:08:57 +0000

    Color outputs created. Animation Data struct created.

---
+++ PPSD/Assets/Scripts/AnimationData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public unsafe struct AnimationData
+{
+    [SerializeField] public fixed float f_vTransform[3];
+    [SerializeField] public fixed float f_vRotation[4];
+
+    public AnimationData(float x, float y, float z, float w, float i, float j, float k)
+    {
+        f_vTransform[0] = x;
+        f_vTransform[1] = y;
+        f_vTransform[2] = z;
+
+        f_vRotation[0] = i;
+        f_vRotation[1] = j;
+        f_vRotation[2] = k;
+        f_vRotation[3] = w;
+    }
+    /// <summary>
+    /// Convert Transform & Quaternion to float 3 & 4
+    /// </summary>
+    /// <param name="_pos">Vertex Position</param>
+    /// <param name="_joint">Joint Rotation</param>
+    public AnimationData(Vector3 _pos, Quaternion _joint)
+    {
+        f_vTransform[0] = _pos.x;
+        f_vTransform[1] = _pos.y;
+        f_vTransform[2] = _pos.z;
+
+        f_vRotation[0] = _joint.x;
+        f_vRotation[1] = _joint.y;
+        f_vRotation[2] = _joint.z;
+        f_vRotation[3] = _joint.w;
+    }
+    public AnimationData(float x, float y, float z, Quaternion _joint)
+    {
+        f_vTransform[0] = x;
+        f_vTransform[1] = y;
+        f_vTransform[2] = z;
+
+        f_vRotation[0] = _joint.x;
+        f_vRotation[1] = _joint.y;
+        f_vRotation[2] = _joint.z;
+        f_vRotation[3] = _joint.w;
+    }
+}
--- PPSD/Assets/Scripts/VertexOutput.cs
+++ PPSD/Assets/Scripts/VertexOutput.cs
@@ -3,19 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VertexOutput : MonoBehaviour
 {
     [SerializeField] private bool vertTest;
+    [SerializeField] private Vector3 v_minRange;
+    [SerializeField] private Vector3 v_maxRange;
     [SerializeField] private SkinnedMeshRenderer smr;
     [SerializeField] Material restMat;
     [SerializeField] Material animat;
+    [SerializeField] private GameObject go_joints;
+    [SerializeField] private AnimationData[] ad_viewableData;
     private int disRow;
     Vector3[] restPose;
     private void Start()
     {
         restPose = smr.sharedMesh.vertices;
     }
+
+    public void GenerateAnimData()
+    {
+        Mesh posedMesh = new Mesh();
+        smr.BakeMesh(posedMesh);
+        Vector3[] _currentPose = posedMesh.vertices;
+        ad_viewableData = CreateAnimData(restPose.Length, _currentPose);
+    }
+
     public void GenerateColourArray()
     {
         // For instance the current pose
@@ -24,31 +38,68 @@ public class VertexOutput : MonoBehaviour
         Vector3[] currentPose = newMesh.vertices;
         int poseWidth = Mathf.RoundToInt(Mathf.Sqrt(restPose.Length));
         int poseHeight = Mathf.Sqrt(restPose.Length) % 2 == 1 ? Mathf.RoundToInt(Mathf.Sqrt(restPose.Length)) : Mathf.RoundToInt(Mathf.Sqrt(restPose.Length)) + 1;
+        // Combined, and seperated colours
         Texture2D defColours = new Texture2D(poseWidth, poseWidth, TextureFormat.RGBA32, false);
+        Texture2D redColours = new Texture2D(poseWidth, poseWidth, TextureFormat.RGBA32, false);
+        Texture2D greenColours = new Texture2D(poseWidth, poseWidth, TextureFormat.RGBA32, false);
+        Texture2D blueColours = new Texture2D(poseWidth, poseWidth, TextureFormat.RGBA32, false);
+        // Create a texture array to draaw the colours to
+        Texture2D[] defs = new Texture2D[4];
+        defs[0] = defColours;
+        defs[1] = redColours; defs[2] = greenColours; defs[3] = blueColours;
+        // Get the colours from the deformations
         Color[,] map = new Color[poseWidth, poseWidth];
-        map = PaintDeformations(poseWidth, poseHeight, currentPose, defColours);
+        map = PaintDeformations(poseWidth, poseHeight, currentPose, defs);
         defColours.Apply();
+        // Output the colours as a material and paint onto the model
         Material colourMap = new Material(Shader.Find("Standard"));
         colourMap.SetTexture("_MainTex", defColours);
         smr.sharedMaterial.EnableKeyword("_MAINTEX");
         smr.sharedMaterial.SetTexture("_MainTex", defColours);
-        File.WriteAllBytes(Application.dataPath + "/Poses2D/displacements.png", defColours.EncodeToPNG());
+        // Write the displacements to file
+        File.WriteAllBytes(Application.dataPath + "/Resources/Poses2D/displacements.png", defColours.EncodeToPNG());
         Debug.Log("Displacements Mapped");
     }
 
-    private Color[,] PaintDeformations(int _i_poseWidth, int _i_poseHeight, Vector3[] _A_currentPose, Texture2D _tex_outputTexture)
+
+
+    private Color[,] PaintDeformations(int _i_poseWidth, int _i_poseHeight, Vector3[] _A_currentPose, Texture2D[] _tex_outputTexture)
     {
         Color[,] newMap = new Color[_i_poseWidth, _i_poseHeight];
         for (int i = 0; i < _i_poseWidth; i++)
         {
             for (int j = 0; j < _i_poseWidth; j++)
             {
-                Color deformation = new Color(_A_currentPose[i].x - restPose[i].x, _A_currentPose[i].y - restPose[i].y, _A_currentPose[i].z - restPose[i].z);
+                // 
+                Color deformation = new Color((((_A_currentPose[i].x - restPose[i].x) + Mathf.Abs(v_minRange.x)) / (v_maxRange.x - v_minRange.x)),
+                    (((_A_currentPose[i].y - restPose[i].y) + Mathf.Abs(v_minRange.y)) / (v_maxRange.y - v_minRange.y)),
+                    (((_A_currentPose[i].z - restPose[i].z) + Mathf.Abs(v_maxRange.z)) / (v_maxRange.z - v_minRange.z)));
+                Color redDef = new Color((((_A_currentPose[i].x - restPose[i].x) + Mathf.Abs(v_minRange.x)) / (v_maxRange.x - v_minRange.x)), 0.0f, 0.0f);
+                Color greenDef = new Color(0.0f, (((_A_currentPose[i].y - restPose[i].y) + Mathf.Abs(v_minRange.y)) / (v_maxRange.y - v_minRange.z)), 0.0f);
+                Color blueDef = new Color(0.0f, 0.0f, (((_A_currentPose[i].z - restPose[i].z) + Mathf.Abs(v_minRange.z)) / (v_maxRange.z - v_minRange.z)));
+
                 newMap[i, j] = deformation;
-                _tex_outputTexture.SetPixel(i, j, newMap[i, j]);
+                _tex_outputTexture[0].SetPixel(i, j, newMap[i, j]);
             }
         }
         return newMap;
+    }
+
+    private AnimationData[] CreateAnimData(int _size, Vector3[] _currentVerts)
+    {
+        AnimationData[] _animData = new AnimationData[_size];
+        for (int i = 0; i < _size; i++)
+            _animData[i] = ConvertToAnimData(_currentVerts[i], v_minRange, v_maxRange, restPose[i], Vector3.zero);
+        return _animData;
+    }
+
+    private AnimationData ConvertToAnimData(Vector3 _current, Vector3 _min, Vector3 _max, Vector3 _rest, Vector3 _joint)
+    {
+        Quaternion _jointQuat = Quaternion.Euler(_joint);
+        return new AnimationData(((_current.x - _rest.x) + _min.x) / (_max.x - _min.x),
+            ((_current.y - _rest.y) + _min.x) / (_max.y - _min.y),
+            ((_current.z - _rest.z) + _min.z)/ (_max.z - _min.z),
+            _jointQuat);
     }
 
     private void PaintUVs()
